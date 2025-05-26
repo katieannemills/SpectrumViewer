4ff36cad4e0ecddcfbf2965bcf6c9e997acfc126
bkatiemills <you@example.com>
2025-05-26 12:47:38 -0400

    draw polygons on plotly plot

--- scripts/plotly-heatmap.js
+++ scripts/plotly-heatmap.js
@@ -1,18 +1,61 @@
 function plotly_hm(div){
     this.div = div;
+    this.polyX = [];
+    this.polyY = [];
+    this.lastZ = null;
+    this.shiftDown = false;
 
-    this.draw = function(z){
-        const data = [{
+    // Track shift key - plotly_click event does not provide shift key state :(
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'Shift') this.shiftDown = true;
+    });
+    document.addEventListener('keyup', (e) => {
+        if (e.key === 'Shift') this.shiftDown = false;
+    });
+
+    this.draw = (z) => {
+        this.lastZ = z
+
+        const data = {
             z: z,
             type: 'heatmap',
             colorscale: 'Viridis',
             hoverongaps: false
-        }];
+        };
 
         const layout = {
             title: 'demo',
         };
 
-        Plotly.newPlot(this.div, data, layout);
+        // close polygon where applicable
+        let polyX = [...this.polyX];
+        let polyY = [...this.polyY];
+        if (polyX.length >= 3) {
+            polyX.push(polyX[0]);
+            polyY.push(polyY[0]);
+        }
+
+        const polygon = {
+            x: polyX,
+            y: polyY,
+            mode: 'lines',
+            line: { color: 'white' },
+            type: 'scatter',
+            name: 'Polygon',
+            showlegend: false
+        };
+
+        Plotly.newPlot(this.div, [data, polygon], layout).then(() => {
+                document.getElementById(this.div).on('plotly_click', (event) => {
+                    if (!this.shiftDown) return;
+
+                    const pt = event.points[0];
+                    this.polyX.push(pt.x);
+                    this.polyY.push(pt.y);
+
+                    this.draw(this.lastZ);
+                });
+                this.eventsBound = true;
+        });
     }
 }
